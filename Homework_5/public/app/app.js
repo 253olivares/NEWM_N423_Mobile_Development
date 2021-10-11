5cbd9067771bfa0a520734f9a21c14febb0a94b0
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2021-10-11 13:45:57 -0400

    saving work

--- Homework_5/public/app/app.js
+++ Homework_5/public/app/app.js
@@ -1,37 +1,42 @@
 var _db;
 
-var albumDisplay = "";
-
 function initFirebase() {
-    firebase.auth().signInAnonymously()
-    .then(() => {
-      _db = firebase.firestore();
-    })
-    .catch((error) => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      _db = [];
-    });
-}
+    firebase
+      .auth()
+      .signInAnonymously()
+      .then(() => {
+        // signing in anonymously
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        _db = [];
+      });
+    _db = firebase.firestore();
+  }
 
-function displayAlbums(){
+function loadAlbum(doc){
     _db
     .collection("Albums")
     .get()
     .then(function(querySnapshot){
         querySnapshot.forEach(function(doc){
-            console.log(doc.data())
+            console.log(doc.data());
         });
-    });
+    }, function(error){
+        console.log("Error:", error);
+    })
 }
 
 
-$(document).ready(function () {
-    try{
+
+
+$(document).ready(function(){
+    try {
         let app = firebase.app();
         initFirebase();
-        displayAlbums();
-    } catch {
-        console.error(e);
-    }
-});
+        loadAlbum();
+      } catch {
+        console.error("failed to load functions");
+      }
+})
