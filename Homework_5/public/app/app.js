2c2b13ec4b452dc1c4cfaf01097ca16132846cc5
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2021-10-11 13:33:53 -0400

    updated stuff

--- Homework_5/public/app/app.js
+++ Homework_5/public/app/app.js
@@ -1,11 +1,37 @@
-import * as Model from "../model/model.js";
+var _db;
 
-function initListeners() {
+var albumDisplay = "";
 
+function initFirebase() {
+    firebase.auth().signInAnonymously()
+    .then(() => {
+      _db = firebase.firestore();
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      _db = [];
+    });
+}
+
+function displayAlbums(){
+    _db
+    .collection("Albums")
+    .get()
+    .then(function(querySnapshot){
+        querySnapshot.forEach(function(doc){
+            console.log(doc.data())
+        });
+    });
 }
 
 
 $(document).ready(function () {
-    Model.initFirebase();
-    Model.signIn(initListeners);
+    try{
+        let app = firebase.app();
+        initFirebase();
+        displayAlbums();
+    } catch {
+        console.error(e);
+    }
 });
